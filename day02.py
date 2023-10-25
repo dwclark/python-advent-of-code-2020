885df420d470c47c6818b9a045e20ce10c9303ae
David Clark <me@nobody.com>
2023-10-24 20:21:58 -0500

    Passing lambda to common count function

--- day02.py
+++ day02.py
@@ -11,17 +11,8 @@ def all_passwords():
 
     return [as_tuple(line) for line in non_blank_lines('input/day02.txt')]
 
-def part_1():
-    def is_valid(info):
-        return info.num1 <= info.pwd.count(info.look_for) <= info.num2
-    
+def count(is_valid):
     return [is_valid(password) for password in all_passwords()].count(True)
 
-def part_2():
-    def is_valid(info):
-        return (info.pwd[info.num1-1] == info.look_for) ^ (info.pwd[info.num2-1] == info.look_for)
-    
-    return [is_valid(password) for password in all_passwords()].count(True)
-
-print("Part 1: ", part_1())
-print("Part 2: ", part_2())
+print("Part 1: ", count(lambda info: info.num1 <= info.pwd.count(info.look_for) <= info.num2))
+print("Part 2: ", count(lambda info: (info.pwd[info.num1-1] == info.look_for) ^ (info.pwd[info.num2-1] == info.look_for)))
