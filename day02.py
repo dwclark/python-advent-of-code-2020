6fb1b5ccf6dafc5079243a56d5c2f0073f166300
David Clark <me@nobody.com>
2023-10-24 20:17:35 -0500

    Converted to use more idiomatic list comprehension

--- day02.py
+++ day02.py
@@ -9,16 +9,19 @@ def as_tuple(line):
     return Pwd(int(low), int(high), c.replace(':',''), s)
 
 def all_passwords():
-    return list(map(as_tuple, non_blank_lines('input/day02.txt')))
+    return [as_tuple(line) for line in non_blank_lines('input/day02.txt')]
 
 def part_1():
     def is_valid(info):
         return info.num1 <= info.pwd.count(info.look_for) <= info.num2
     
-    return list(map(is_valid, all_passwords())).count(True)
+    return [is_valid(password) for password in all_passwords()].count(True)
 
 def part_2():
     def is_valid(info):
         return (info.pwd[info.num1-1] == info.look_for) ^ (info.pwd[info.num2-1] == info.look_for)
     
-    return list(map(is_valid, all_passwords())).count(True)
+    return [is_valid(password) for password in all_passwords()].count(True)
+
+print("Part 1: ", part_1())
+print("Part 2: ", part_2())
