9690404dd3cd4c547164c6cd20d270c38741c245
David Clark <me@nobody.com>
2023-10-24 20:31:40 -0500

    Converted maps to list comprehensions

--- day06.py
+++ day06.py
@@ -4,8 +4,7 @@ from functools import reduce
 groups = blank_line_grouped('input/day06.txt')
 
 def solve(reducer):
-    return sum(map(lambda s: len(s),
-                  map(lambda group: reduce(reducer, group), groups)))
+    return sum([len(s) for s in [reduce(reducer, group) for group in groups]])
 
 print("Part 1: ", solve(lambda one, two: set(one) | set(two)))
 print("Part 2: ", solve(lambda one, two: set(one) & set(two)))
