2980af05de02e6f0f498c970b30f2fbc94c1008e
David Clark <me@nobody.com>
2023-11-01 16:10:02 -0500

    Now using print_assert, made lambdas

--- day05.py
+++ day05.py
@@ -1,15 +1,9 @@
-from aoc import non_blank_lines
+from aoc import non_blank_lines, print_assert
 
 def load_all():
-    def row(s):
-        return int(s[0:7].replace('F', '0').replace('B', '1'), base=2)
-
-    def column(s):
-        return int(s[7:].replace('R', '1').replace('L', '0'), base=2)
-
-    def seat_id(s):
-        return (row(s) * 8) + column(s)
-
+    row = lambda s: int(s[0:7].replace('F', '0').replace('B', '1'), base=2)
+    column = lambda s: int(s[7:].replace('R', '1').replace('L', '0'), base=2)
+    seat_id = lambda s: (row(s) * 8) + column(s)
     return sorted([seat_id(line) for line in non_blank_lines("input/day05.txt")])
 
 all_ids = load_all()
@@ -19,5 +13,5 @@ def part_2():
         if all_ids[idx] + 1 != all_ids[idx+1]:
             return (all_ids[idx] + 1)
 
-print("Part 1: ", max(all_ids))
-print("Part 2: ", part_2())
+print_assert("Part 1:", max(all_ids), 816)
+print_assert("Part 2:", part_2(), 539)
