f4c929967fca0181350674b33b91346387232aa3
David Clark <me@nobody.com>
2023-11-01 11:11:14 -0500

    Added print_assert, made day 01 use generators

--- aoc.py
+++ aoc.py
@@ -32,3 +32,7 @@ def set_bit(value, bit):
 
 def clear_bit(value, bit):
     return value & ~(1<<bit)
+
+def print_assert(preamble, val, should_be):
+    assert val == should_be
+    print(preamble, val)
--- day01.py
+++ day01.py
@@ -1,18 +1,17 @@
-from aoc import single_int_line as get_nums
+from aoc import non_blank_lines, print_assert
+import math
 
-def part_1(nums):
-    for outer in range(0, len(nums)-1):
-        for inner in range(1, len(nums)):
-            if (nums[inner] + nums[outer]) == 2020:
-                return nums[inner] * nums[outer]
+n = [ int(s) for s in non_blank_lines('input/day01.txt') ]
 
-def part_2(nums):
-    for outer in range(0, len(nums)-2):
-        for middle in range(1, len(nums)-1):
-            for inner in range(2, len(nums)):
-                if (nums[inner] + nums[middle] + nums[outer]) == 2020:
-                    return nums[inner] * nums[middle] * nums[outer]
+gen_1 = ((n[outer], n[inner]) for outer in range(0, len(n)-1) for inner in range(1, len(n))
+         if n[outer] + n[inner] == 2020)
 
-n = get_nums('input/day01.txt')
-print("part_1: ", part_1(n))
-print("part 2: ", part_2(n))
+gen_2 = ((n[outer], n[middle], n[inner])
+         for outer in range(0, len(n)-2)
+         for middle in range(1, len(n)-1)
+         for inner in range(2, len(n))
+         if n[outer] + n[middle] + n[inner] == 2020)
+
+print_assert("Part 1:", math.prod(next(gen_1)), 956091)
+print_assert("Part 2:", math.prod(next(gen_2)), 79734368)
+
